75fbfee789f2092dc9c21faf608b892d1f9e5317
mahbub-it <50598891+mahbub-it@users.noreply.github.com>
2025-12-07 14:51:52 +0600

    After some update

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -16,13 +16,21 @@ class LoginController extends Controller
 
     public function registerPost(Request $request)
     {
-        $user = new User;
-        $user->name = $request->name;
-        $user->username = $request->username;
-        $user->photo = $request->photo;
-        $user->email = $request->email;
-        $user->password = bcrypt($request->password);
-        $user->save();
+        $user = User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'photo' => $request->photo,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
         return redirect('/register')->with('message', 'User Created Successfully');
+        // $user = new User;
+        // $user->name = $request->name;
+        // $user->username = $request->username;
+        // $user->photo = $request->photo;
+        // $user->email = $request->email;
+        // $user->password = bcrypt($request->password);
+        // $user->save();
+        // return redirect('/register')->with('message', 'User Created Successfully');
     }
 }
