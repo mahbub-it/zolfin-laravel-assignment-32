6f441acabdeda8441b7b30f6c1b7325954d50cb3
mahbub-it <bdsmrahman@gmail.com>
2026-02-04 13:28:37 +0600

    Class 33

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,24 +3,26 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Faker\Factory;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
     public function run()
     {
-        $faker = Factory::create();
+        // $faker = Factory::create();
 
-        for ($i = 0; $i < 10; $i++) {
-            DB::table('users')->insert([
-                'name' => $faker->name(),
-                'username' => $faker->username(),
-                'photo' => $faker->imageUrl(100, 100),
-                'email' => $faker->safeEmail(),
-                'password' => Hash::make('pass123')
-            ]);
-        }
+        // for ($i = 0; $i < 10; $i++) {
+        //     DB::table('users')->insert([
+        //         'name' => $faker->name(),
+        //         'username' => $faker->username(),
+        //         'photo' => $faker->imageUrl(100, 100),
+        //         'email' => $faker->safeEmail(),
+        //         'password' => Hash::make('pass123')
+        //     ]);
+        // }
+
+        $user = new User;
+
+        $user->factory(10)->create();
     }
 }
